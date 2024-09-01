be359c994da843ddaac1cc5a6066467503e6c77e
naumanch969 <naumanch969@gmail.com>
2024-09-01 12:29:53 +0500

    Auth screens setup

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+import AuthScreen from '@/features/auth/comonents/auth-screen'
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <AuthScreen />
+  )
+}
+
+export default AuthPage
